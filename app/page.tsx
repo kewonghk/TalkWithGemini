14d140c320f7d800fcf53d6f6b679e9ab1a731b8
kewonghk <kewong@gmail.com>
2024-06-20 11:04:18 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -587,11 +587,11 @@ export default function Home() {
           <div className="ml-3 font-bold leading-10 max-sm:leading-8">{t('title')}</div>
         </div>
         <div className="flex items-center gap-1">
-          <!-----<Button title={t('github')} variant="ghost" size="icon" className="h-8 w-8">
+          {/*}<Button title={t('github')} variant="ghost" size="icon" className="h-8 w-8">
             <a href="https://github.com/Amery2010/TalkWithGemini" target="_blank">
               <Github className="h-5 w-5" />
             </a>
-          </Button>----->
+          </Button>*/}
           <ThemeToggle />
           <Button
             className="h-8 w-8"
